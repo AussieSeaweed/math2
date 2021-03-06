c74eb0ed468fef8cf1be053fd6a5fce044c5bdee
Juho <juho-kim@outlook.com>
2021-03-05 20:43:43 -0500

    rearranged imports

--- math2/linalg/__init__.py
+++ math2/linalg/__init__.py
@@ -1,4 +1,4 @@
 from math2.linalg.data import Vector
-from math2.linalg.funcs import ones, zeros
+from math2.linalg.funcs import full, ones, replaced, zeros
 
-__all__ = 'Vector', 'ones', 'zeros'
+__all__ = 'Vector', 'full', 'ones', 'replaced', 'zeros'
