0431ea6b93faec2e50062ba73a71efa3bcd9c8e5
Juho <juho-kim@outlook.com>
2021-03-17 22:27:58 -0400

    optimized imports

--- math2/stats/__init__.py
+++ math2/stats/__init__.py
@@ -1,12 +1,11 @@
 from math2.stats.analysis import mean, median, range_, standard_deviation, trimmed_mean, variance
 from math2.stats.counting import combinations, cyclic_permutations, factorial, partitions, permutations
-from math2.stats.dists import (BinomialDistribution, DiscreteDistribution, Distribution, GeometricDistribution,
-                               HypergeometricDistribution, NegativeBinomialDistribution, PoissonDistribution,
-                               ContinuousDistribution, UniformDistribution, NormalDistribution,
-                               StandardNormalDistribution)
+from math2.stats.dists import (BinomialDistribution, ContinuousDistribution, DiscreteDistribution, Distribution,
+                               GeometricDistribution, HypergeometricDistribution, NegativeBinomialDistribution,
+                               NormalDistribution, PoissonDistribution, StandardNormalDistribution, UniformDistribution)
 
 __all__ = ('mean', 'median', 'range_', 'standard_deviation', 'trimmed_mean', 'variance', 'combinations',
            'cyclic_permutations', 'factorial', 'partitions', 'permutations', 'BinomialDistribution',
-           'DiscreteDistribution', 'Distribution', 'GeometricDistribution', 'HypergeometricDistribution',
-           'NegativeBinomialDistribution', 'PoissonDistribution', 'ContinuousDistribution', 'UniformDistribution',
-           'NormalDistribution', 'StandardNormalDistribution')
+           'ContinuousDistribution', 'DiscreteDistribution', 'Distribution', 'GeometricDistribution',
+           'HypergeometricDistribution', 'NegativeBinomialDistribution', 'NormalDistribution', 'PoissonDistribution',
+           'StandardNormalDistribution', 'UniformDistribution')
