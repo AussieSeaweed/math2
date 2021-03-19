de2ea752e42bc26c720f7b506cd804f76dff4868
Juho <juho-kim@outlook.com>
2021-03-18 22:59:49 -0400

    Added tensors

--- math2/linalg/tensors.py
+++ math2/linalg/tensors.py
@@ -1,4 +1,7 @@
 from collections.abc import MutableSequence
+from functools import partial
+from itertools import starmap
+from operator import add, div, mul, neg, pos, sub
 
 
 class Vector(MutableSequence):
@@ -7,6 +10,48 @@ class Vector(MutableSequence):
 
     def insert(self, index, value):
         self.__values.insert(index, value)
+
+    def __pos__(self):
+        return Vector(map(pos, self))
+
+    def __neg__(self):
+        return Vector(map(neg, self))
+
+    def __add__(self, other):
+        try:
+            if len(self) == len(other):
+                return Vector(starmap(add, zip(self, other)))
+            else:
+                raise ValueError('Invalid lengths')
+        except TypeError:
+            return NotImplemented
+
+    def __sub__(self, other):
+        try:
+            if len(self) == len(other):
+                return Vector(starmap(sub, zip(self, other)))
+            else:
+                raise ValueError('Invalid lengths')
+        except TypeError:
+            return NotImplemented
+
+    def __mul__(self, other):
+        return Vector(map(partial(mul, other), self))
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return Vector(map(partial(div, other), self))
+
+    def __matmul__(self, other):
+        try:
+            if len(self) == len(other):
+                return sum(map(mul, zip(self, other)))
+            else:
+                raise ValueError('Invalid lengths')
+        except TypeError:
+            return NotImplemented
 
     def __getitem__(self, i):
         return self.__values[i]
