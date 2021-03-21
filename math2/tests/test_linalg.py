bedcb08d773d07d88e4ee7a237e21212a12d609c
Juho <juho-kim@outlook.com>
2021-03-20 22:55:26 -0400

    simplified tests

--- math2/tests/test_linalg.py
+++ math2/tests/test_linalg.py
@@ -10,68 +10,67 @@ from math2.linalg import DimensionError, Matrix, column, row, singleton, transpo
 class LinAlgTestCase(ExtendedTestCase):
     def test_abs(self) -> None:
         self.assertAlmostEqual(abs(Matrix((range(3), range(3, 6)))), 7.416198487095663)
-        self.assertAlmostEqual(abs(Matrix((range(6),))), 7.416198487095663)
-        self.assertAlmostEqual(abs(Matrix((x,) for x in range(6))), 7.416198487095663)
+        self.assertAlmostEqual(abs(row(range(6))), 7.416198487095663)
+        self.assertAlmostEqual(abs(column(range(6))), 7.416198487095663)
 
     def test_iter(self) -> None:
         self.assertEqual(Matrix((range(3), range(3, 6))), range(6))
-        self.assertEqual(Matrix((range(6),)), range(6))
-        self.assertEqual(Matrix((x,) for x in range(6)), range(6))
+        self.assertEqual(row(range(6)), range(6))
+        self.assertEqual(column(range(6)), range(6))
 
     def test_getitem(self) -> None:
         self.assertEqual(Matrix((range(3), range(3, 6)))[4], 4)
-        self.assertEqual(Matrix((range(6),))[4], 4)
-        self.assertEqual(Matrix((x,) for x in range(6))[4], 4)
+        self.assertEqual(row(range(6))[4], 4)
+        self.assertEqual(column(range(6))[4], 4)
 
         self.assertIterableEqual(Matrix((range(3), range(3, 6)))[2:5], range(2, 5))
-        self.assertIterableEqual(Matrix((range(6),))[2:5], range(2, 5))
-        self.assertIterableEqual(Matrix((x,) for x in range(6))[2:5], range(2, 5))
+        self.assertIterableEqual(row(range(6))[2:5], range(2, 5))
+        self.assertIterableEqual(column(range(6))[2:5], range(2, 5))
 
         self.assertIterableEqual(Matrix((range(3), range(3, 6)))[::-1], reversed(range(6)))
-        self.assertIterableEqual(Matrix((range(6),))[::-1], reversed(range(6)))
-        self.assertIterableEqual(Matrix((x,) for x in range(6))[::-1], reversed(range(6)))
+        self.assertIterableEqual(row(range(6))[::-1], reversed(range(6)))
+        self.assertIterableEqual(column(range(6))[::-1], reversed(range(6)))
 
         self.assertEqual(Matrix((range(3), range(3, 6)))[1, 1], 4)
-        self.assertEqual(Matrix((range(6),))[0, 4], 4)
-        self.assertEqual(Matrix((x,) for x in range(6))[4, 0], 4)
+        self.assertEqual(row(range(6))[0, 4], 4)
+        self.assertEqual(column(range(6))[4, 0], 4)
 
         self.assertEqual(Matrix((range(3), range(3, 6)))[:, :], Matrix((range(3), range(3, 6))))
-        self.assertEqual(Matrix((range(6),))[:, :], Matrix((range(6),)))
-        self.assertEqual(Matrix((x,) for x in range(6))[:, :], Matrix((x,) for x in range(6)))
+        self.assertEqual(row(range(6))[:, :], row(range(6)))
+        self.assertEqual(column(range(6))[:, :], column(range(6)))
 
         self.assertEqual(Matrix((range(3), range(3, 6)))[:, :2], Matrix((range(2), range(3, 5))))
-        self.assertEqual(Matrix((range(6),))[0, :], Matrix((range(6),)))
-        self.assertEqual(Matrix((x,) for x in range(6))[:, 0], Matrix((x,) for x in range(6)))
+        self.assertEqual(row(range(6))[0, :], row(range(6)))
+        self.assertEqual(column(range(6))[:, 0], column(range(6)))
 
     def test_len(self) -> None:
         self.assertEqual(len(Matrix((range(3), range(3, 6)))), 6)
-        self.assertEqual(len(Matrix((range(6),))), 6)
-        self.assertEqual(len(Matrix((x,) for x in range(6))), 6)
+        self.assertEqual(len(row(range(6))), 6)
+        self.assertEqual(len(column(range(6))), 6)
 
     def test_pos(self) -> None:
         self.assertEqual(+Matrix((range(3), range(3, 6))), Matrix((range(3), range(3, 6))))
-        self.assertEqual(+Matrix((range(6),)), Matrix((range(6),)))
-        self.assertEqual(+Matrix((x,) for x in range(6)), Matrix((x,) for x in range(6)))
+        self.assertEqual(+row(range(6)), row(range(6)))
+        self.assertEqual(+column(range(6)), column(range(6)))
 
     def test_neg(self) -> None:
         self.assertEqual(-Matrix((range(3), range(3, 6))), Matrix((range(0, -3, -1), range(-3, -6, -1))))
-        self.assertEqual(-Matrix((range(6),)), Matrix((range(0, -6, -1),)))
-        self.assertEqual(-Matrix((x,) for x in range(6)), Matrix((-x,) for x in range(6)))
+        self.assertEqual(-row(range(6)), Matrix((range(0, -6, -1),)))
+        self.assertEqual(-column(range(6)), Matrix((-x,) for x in range(6)))
 
     def test_add(self) -> None:
         self.assertEqual(Matrix((range(3), range(3, 6))) + Matrix((range(6, 9), range(9, 12))),
                          Matrix(((6, 8, 10), (12, 14, 16))))
-        self.assertEqual(Matrix((range(5, -1, -1),)) + Matrix((range(6),)), Matrix((repeat(5, 6),)))
-        self.assertEqual(Matrix((x,) for x in range(5, -1, -1)) + Matrix((x,) for x in range(6)),
-                         Matrix((5,) for _ in range(6)))
+        self.assertEqual(row(range(5, -1, -1)) + row(range(6)), row(repeat(5, 6)))
+        self.assertEqual(column(range(5, -1, -1)) + column(range(6)), column(repeat(5, 6)))
         self.assertRaises(TypeError, add, row(range(5)), 5)
         self.assertRaises(DimensionError, add, row(range(5)), column(range(5)))
         self.assertRaises(DimensionError, add, row(range(5)), row(range(6)))
 
     def test_mul(self) -> None:
         self.assertEqual(Matrix((range(3), range(3, 6))) * 5, Matrix((range(0, 15, 5), range(15, 30, 5))))
-        self.assertEqual(Matrix((range(5, -1, -1),)) * 5, row(range(25, -1, -5)))
-        self.assertEqual(Matrix((x,) for x in range(5, -1, -1)) * 5, column(range(25, -1, -5)))
+        self.assertEqual(row(range(5, -1, -1)) * 5, row(range(25, -1, -5)))
+        self.assertEqual(column(range(5, -1, -1)) * 5, column(range(25, -1, -5)))
 
         self.assertEqual(Matrix((range(3), range(3, 6))) * Matrix((range(2), range(2, 4), range(4, 6))),
                          Matrix(((10, 13), (28, 40))))
@@ -84,13 +83,13 @@ class LinAlgTestCase(ExtendedTestCase):
 
     def test_rmul(self) -> None:
         self.assertEqual(5 * Matrix((range(3), range(3, 6))), Matrix((range(0, 15, 5), range(15, 30, 5))))
-        self.assertEqual(5 * Matrix((range(5, -1, -1),)), row(range(25, -1, -5)))
-        self.assertEqual(5 * Matrix((x,) for x in range(5, -1, -1)), column(range(25, -1, -5)))
+        self.assertEqual(5 * row(range(5, -1, -1)), row(range(25, -1, -5)))
+        self.assertEqual(5 * column(range(5, -1, -1)), column(range(25, -1, -5)))
 
     def test_matmul(self) -> None:
         self.assertEqual(Matrix((range(3), range(3, 6))) @ Matrix((range(6, 9), range(9, 12))), 145)
-        self.assertEqual(Matrix((range(5, -1, -1),)) @ Matrix((range(6),)), 20)
-        self.assertEqual(Matrix((x,) for x in range(5, -1, -1)) @ Matrix((x,) for x in range(6)), 20)
+        self.assertEqual(row(range(5, -1, -1)) @ row(range(6)), 20)
+        self.assertEqual(column(range(5, -1, -1)) @ column(range(6)), 20)
         self.assertRaises(DimensionError, matmul, row(range(5)), column(range(5)))
         self.assertRaises(DimensionError, matmul, row(range(5)), row(range(6)))
 
@@ -98,8 +97,8 @@ class LinAlgTestCase(ExtendedTestCase):
 class UtilsTestCase(ExtendedTestCase):
     def test_transposed(self):
         self.assertEqual(transposed(Matrix((range(3), range(3, 6)))), Matrix(((0, 3), (1, 4), (2, 5))))
-        self.assertEqual(transposed(Matrix((range(6),))), column(range(6)))
-        self.assertEqual(transposed(Matrix((x,) for x in range(6))), row(range(6)))
+        self.assertEqual(transposed(row(range(6))), column(range(6)))
+        self.assertEqual(transposed(column(range(6))), row(range(6)))
 
 
 if __name__ == '__main__':
