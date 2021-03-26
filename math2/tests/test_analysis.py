6d1eceb9c8bebcaaa005a774198baff72fcd5958
Juho <juho-kim@outlook.com>
2021-03-25 20:23:28 -0400

    verified integrators

--- math2/tests/test_analysis.py
+++ math2/tests/test_analysis.py
@@ -19,6 +19,7 @@ class IntegratorsTestCase(ExtendedTestCase):
         self.assertAlmostEqual(integrate(sin, 0, 2 * pi, steps=100, integrator=TrapezoidIntegrator()), 0)
         self.assertAlmostEqual(integrate(sin, 0, 2 * pi, steps=100, integrator=SimpsonIntegrator()), 0)
 
+    def test_double_integrate(self) -> None:
         self.assertAlmostEqual(double_integrate(
             lambda x, y: x * y ** 2, 0, 1, lambda x: 0, pos, steps=100, integrator=MidpointIntegrator()), 1 / 15, 3)
         self.assertAlmostEqual(double_integrate(
@@ -32,19 +33,17 @@ class IntegratorsTestCase(ExtendedTestCase):
         self.assertAlmostEqual(double_integrate(
             lambda x, y: x ** 2 * y, 0, 1, lambda x: 0, pos, steps=100, integrator=SimpsonIntegrator()), 0.1)
 
+    def test_triple_integrate(self) -> None:
         self.assertAlmostEqual(triple_integrate(
-            lambda x, y, z: sin(x) * cos(y) * z,
-            -5, 1, lambda x: -10, pos, lambda x, y: -x * y * 2, mul,
+            lambda x, y, z: sin(x) * cos(y) * z, -5, 1, lambda x: -10, pos, lambda x, y: -x * y * 2, mul,
             steps=50, integrator=MidpointIntegrator(),
         ), 1289.4240604730014, -1)
         self.assertAlmostEqual(triple_integrate(
-            lambda x, y, z: sin(x) * cos(y) * z,
-            -5, 1, lambda x: -10, pos, lambda x, y: -x * y * 2, mul,
+            lambda x, y, z: sin(x) * cos(y) * z, -5, 1, lambda x: -10, pos, lambda x, y: -x * y * 2, mul,
             steps=50, integrator=TrapezoidIntegrator(),
         ), 1289.4240604730014, -1)
         self.assertAlmostEqual(triple_integrate(
-            lambda x, y, z: sin(x) * cos(y) * z,
-            -5, 1, lambda x: -10, pos, lambda x, y: -x * y * 2, mul,
+            lambda x, y, z: sin(x) * cos(y) * z, -5, 1, lambda x: -10, pos, lambda x, y: -x * y * 2, mul,
             steps=50, integrator=SimpsonIntegrator(),
         ), 1289.4240604730014, 3)
 
